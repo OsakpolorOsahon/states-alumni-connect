cd7b762df77a5e107e42d82ec70816bb1cc9844d
OsakpolorOsahon <osahon1224@gmail.com>
2025-07-16 12:11:11 +0100

    Update uploadthing.ts

--- client/src/lib/uploadthing.ts
+++ client/src/lib/uploadthing.ts
@@ -1,5 +1,5 @@
 import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react";
 import type { UploadRouter } from "../../../server/uploadthing";
 
-export // Removed broken hooks
-export // Removed broken hooks
+// Removed broken hooks
+// Removed broken hooks
